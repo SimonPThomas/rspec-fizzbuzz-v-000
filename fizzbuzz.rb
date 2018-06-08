cf4bcea2b3bd8d32202e95277f30263005ddb348
Simon Thomas <simonpthomas89@gmail.com>
2018-06-08 18:07:05 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,15 @@
+int =  ' '
+def fizzbuzz (int)
+  if int % 3 == 0
+    puts "Fizz"
+end
+ if int % 5 == 0
+   puts "Buzz"
+end
+ if int % 3 && 5 == 0
+   puts "FizzBuzz"
+end
+ if int !% 3 || !% 5 == 0
+   puts "Nil"
+  end
+end
