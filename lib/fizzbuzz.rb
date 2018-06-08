d333ed8894f77e44f142db6361ea6668007cb9ee
Simon Thomas <simonpthomas89@gmail.com>
2018-06-08 18:02:09 +0000

    automatically backed up by learn

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -2,15 +2,14 @@ int = ' '
 def fizzbuzz (int)
   if int % 3 == 0
     puts "Fizz"
-
- elsif int % 5 == 0
+end
+ if int % 5 == 0
    puts "Buzz"
-
- elsif int % 3 && 5 == 0
+end 
+ if int % 3 && 5 == 0
    puts "FizzBuzz"
-
- else int !% 3 || !% 5 == 0
+end
+ if int !% 3 || !% 5 == 0
    puts "Nil"
-
   end
 end
